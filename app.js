8496527613464c21402407cacfa45d99f86bc9e9
Qistina Illya <qistinillya@yahoo.com>
2021-02-04 19:53:25 +0800

    adding vue js

---
+++ app.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+Vue.createApp({
+    // data: function()
+    data(){
+        return{
+            goals: [],
+            enteredValue:''
+        };
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue); //to display the value
+            this.enteredValue =''; //to reset the input box
+        }
+    }
+}).mount('#app'); //to assign to which part should the data will work on
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal(){
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
