4ad13063ed1809a1f38c62d9d88e3d9d9f154394
Qistina Illya <qistinillya@yahoo.com>
2021-02-23 15:54:49 +0800

    intro for computed and watcher

--- Third Project/app.js
+++ Third Project/app.js
@@ -2,43 +2,78 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 15,
-      name:'',
-      confirmedName:''
-
+      name: "",
+      lastname: '',
+      confirmedName: "",
+      // fullname: "",
     };
   },
-  methods: {
-    outputFullname(){
-      if(this.name ===''){
-        return '';
+
+  watch: {
+    // need to use the same as data to be used as a method/function to get the value of the data
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     // we do not need return when using watcher
+    //     this.fullname = value + " " + this.lastname;
+    //   }
+    // },
+
+    // lastname(value){
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }
+    //   else{
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
+  },
+  // for perfomance
+  computed: {
+    // not gonna use it ad a function but it is a function. Bsecause we going to use it as a data. Only use computed when we want to display something only, like in this case, name
+    fullname(){
+      console.log('Running again...');
+      if (this.name === "" || this.lastname === '') {
+        return "";
       }
-      return this.name + ' ' + 'Azhar';
+      return this.name + " " + this.lastname;
+    }
+  },
+  // use method when we want to recalculate again, if not, use computed
+  methods: {
+    outputFullname() {
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Azhar";
     },
     add(num) {
-      this.counter = this.counter + num;;
+      this.counter = this.counter + num;
     },
-    remove(num){
-      this.counter = this.counter - num;;
+    remove(num) {
+      this.counter = this.counter - num;
     },
-    setName(event){
+    setName(event) {
       //auto update the input
       this.name = event.target.value;
     },
-    submitForm(){
-      alert('Submitted');
+    submitForm() {
+      alert("Submitted");
     },
-    confirmed(){
+    confirmed() {
       this.confirmedName = this.name;
     },
-    resetInput(){
-      this.name='';
-    }
+    resetInput() {
+      this.name = "";
+      this.lastname = '';
+    },
     // submitForm(event){
     //   // prevent from reload
-    //   event.preventDefault(); 
+    //   event.preventDefault();
     //   alert('Submitted!');
     // }
-  }
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
