415219ac426cb4fc45cee8c9f10d4d2521a092a3
Qistina Illya <qistinillya@yahoo.com>
2021-02-24 11:58:39 +0800

    assignment 3

---
+++ Assignment 3/app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+  },
+  computed: {
+    result() {
+      if (this.counter > 37) {
+        return "Too much!";
+      } else if (this.counter < 37) {
+        return "Not there yet";
+      } else {
+        return this.counter;
+      }
+    },
+  },
+  watch: {
+    result() {
+      const that = this;
+      setTimeout(function () {
+        that.counter = 0;
+      }, 5000);
+    },
+  },
+});
+
+app.mount("#assignment");
