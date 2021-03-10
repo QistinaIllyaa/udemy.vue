b3590f99c33e415cb91f10a2e5b0dadde47eec65
Qistina Illya <qistinillya@yahoo.com>
2021-03-11 00:08:17 +0800

    testing vanilla js

--- Projects/Seventh Project/app.js
+++ Projects/Seventh Project/app.js
@@ -1,18 +1,55 @@
 const app = Vue.createApp({
     data(){
         return{
-            message: 'Test',
-            currentUserInput: ''
+            message: 'Type something in the input box',
+            currentUserInput: '' //at the input box
         };
     },
     methods:{
         setText(){
             this.message = this.currentUserInput;
         },
-        saveInput(event){
+        saveInput(event){ //for input box to be function
             this.currentUserInput = event.target.value;
         }
     }
 });
 
 app.mount('#app');
+
+//... vanila js
+
+let message ='Hello';
+let longMessage = message + ' World';
+console.log(longMessage);
+
+message = 'Hello!!!';
+console.log(longMessage);
+
+// this will give the same output. The new longMessage is not executed
+
+//actual way
+const data ={
+    message: 'Hello',
+    longMessage: 'Hello World!' // lets do longMessage changes when the message changes
+};
+
+const handler = {
+    set(target, key, value){
+        console.log(target); //message value 
+        console.log(key); // the data
+        console.log(value); // value of new message
+
+        if(key == 'message'){
+            target.longMessage = value + 'What';
+        }
+        
+            target.message = value;
+        
+    }
+};
+const proxy = new Proxy (data, handler );
+
+proxy.message = 'Helo!!';
+
+console.log(proxy.longMessage);
